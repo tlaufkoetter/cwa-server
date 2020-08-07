3154bb5b025124f49fdcd08e3bcb999973f8fdac
Kevin Pontes <kevin.pontes@sap.com>
2020-08-07 11:45:16 +0200

    Add padding, multiple keys

--- tools/proto-playground/src/main/java/app/coronawarn/server/tools/protoplayground/SubmissionPayloadGenerator.java
+++ tools/proto-playground/src/main/java/app/coronawarn/server/tools/protoplayground/SubmissionPayloadGenerator.java
@@ -7,6 +7,7 @@ import com.google.protobuf.ByteString;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SubmissionPayloadGenerator {
@@ -17,17 +18,23 @@ public class SubmissionPayloadGenerator {
   }
 
   public void getSubmissionPayload() throws IOException {
-    Key2.Builder key = Key2.newBuilder();
-    key.setKeyData(ByteString.copyFrom(new byte[16]));
-    key.setTransmissionRiskLevel(4);
-    key.setRollingStartNumber(2660760);
-    key.setRollingPeriod(144);
-    key.addAllVisitedCountries(List.of("DE", "FR"));
-    key.setOrigin("DE");
-    key.setVerificationType(VerificationType.LAB_VERIFIED);
+    List<Key2> keys = new ArrayList<>();
+
+    for (int i=0; i<14; i++){
+      Key2.Builder key = Key2.newBuilder();
+      key.setKeyData(ByteString.copyFrom(new byte[16]));
+      key.setTransmissionRiskLevel(4);
+      key.setRollingStartNumber(2657865 + 144 * i);
+      key.setRollingPeriod(144);
+      key.addAllVisitedCountries(List.of("DE", "FR"));
+      key.setOrigin("DE");
+      key.setVerificationType(VerificationType.LAB_VERIFIED);
+      keys.add(key.build());
+    }
 
     SubmissionPayload.Builder submissionPayload = SubmissionPayload.newBuilder();
-    submissionPayload.addKeys(key);
+    submissionPayload.addAllKeys(keys);
+    submissionPayload.setPadding(ByteString.copyFrom(new byte[1000]));
     submissionPayload.build()
         .writeTo(new FileOutputStream("submission_payload.bin"));
   }
